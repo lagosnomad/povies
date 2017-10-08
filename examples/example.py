41726364aa2a2bf87eea6bf817a8a039a3e96565
McZlatan Bourne <bournethesonofthegun@gmail.com>
2017-10-08 19:57:39 +0100

    finished movie class and wrote an example

--- examples/example.py
+++ examples/example.py
@@ -1,7 +1,15 @@
-from povies import Povies
+import povies
 
-p = Povies()
+p = povies.Povies()
 
-uc = p.allmovies()
+# Get movies with a limit
+# print p.movies(10)
 
-print uc
+# Get a movie using the movie_id
+# print p.movie(1)
+
+# # Get movies related to a movie using the movie_id
+# print p.related(1)
+
+# Search for a movie
+print p.search("ninja")
--- povies/povies.py
+++ povies/povies.py
@@ -1,10 +1,60 @@
 import requests
-class Povies:
-    	def __init__(self):
-		pass
 
-	def allmovies(self):
-		url = 'https://yts.ag/api/v2/list_movies.json'
-		res = requests.get(url)
-		dic = res.json()
-		return dic
+class Povies:
+    """
+    This class provides methods for movie requests
+    """
+    def __init__(self):
+        pass
+
+
+    def movies(self, limit):
+        """
+        - List some movies 
+        - Requires a limit
+        - Movies are:
+            * sorted by seeds.
+            * ordered n ascending order
+            * quality of 1080p
+        - Returns 50 movies at max with a default of 20
+        """
+        url = 'https://yts.ag/api/v2/list_movies.json?sort=seeds&limit=%s&order_by=asc&quality=1080p' % limit
+        res = requests.get(url)
+        dic = res.json()
+        return dic["data"]["movies"]
+
+    
+    def search(self, query):
+        """
+        - Search for movies 
+        - Requires a search query
+        - Movies are:
+            * sorted by highest number of downloads.
+            * quality of 1080p
+        """
+        url = 'https://yts.ag/api/v2/list_movies.json?query_term=%s&sort_by=download_count&quality=1080p' % query
+        res = requests.get(url)
+        dic = res.json()
+        return dic["data"]["movies"]
+
+
+    def movie(self, movie_id):
+        """
+        - Fetch a movie detail
+        - Requires a movie_id
+        """
+        url = 'https://yts.ag/api/v2/movie_details.json?movie_id=%s' % movie_id
+        res = requests.get(url)
+        dic = res.json()
+        return dic["data"]["movie"]
+
+
+    def related(self, movie_id):
+        """
+        - Fetch movies related to a movie
+        - Requires a movie_id
+        """
+        url = 'https://yts.ag/api/v2/movie_suggestions.json?movie_id=%s' % movie_id
+        res = requests.get(url)
+        dic = res.json()
+        return dic["data"]["movies"]
